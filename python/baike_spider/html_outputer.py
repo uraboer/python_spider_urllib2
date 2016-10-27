2edc4d2aebbaa96f1bd31e18e39c8a8bf91e8c3a
uraboer <493970240@qq.com>
2016-10-27 10:50:22 +0800

    finish

--- python/baike_spider/html_outputer.py
+++ python/baike_spider/html_outputer.py
@@ -1,8 +1,32 @@
 #coding:utf-8
 
 class HtmlOutputer(object):
-    def collect_data(self, new_urls):
-        pass
+    def __init__(self):
+        self.datas=[]
+
+    def collect_data(self, data):
+        if data is None:
+            return
+        self.datas.append(data)
 
     def output_html(self):
-        pass
+        fout=open('output.html','w')
+
+        fout.write("<html>")
+        fout.write("<meta charset='utf-8'>")
+        fout.write("<body>")
+        fout.write("<table>")
+
+        for data in self.datas:
+            fout.write("<tr>")
+            fout.write("<td>%s</td>" %data['url'])
+            fout.write("<td>%s</td>" %data['title'].encode("utf-8"))
+            fout.write("<td>%s</td>" %data['summary'].encode("utf-8"))
+
+            fout.write("<tr>")
+
+        fout.write("</table>")
+        fout.write("</html>")
+        fout.write("</body>")
+
+        fout.close()
--- python/baike_spider/spider_main.py
+++ python/baike_spider/spider_main.py
@@ -1,46 +1,38 @@
-#coding:utf-8
-from baike_spider import html_downloader
-from baike_spider import html_outputer
-from baike_spider import html_parser
-from baike_spider import url_manager
+# coding:utf8
 
+from baike_spider import url_manager, html_downloader, html_parser, html_outputer
 
 class SpiderMain(object):
-    def __init__(self):  #初始化
-        self.urls=url_manager.UrlManager()
-        self.downloader=html_downloader.HtmlDownloader()
-        self.parser=html_parser.HtmlParser()
-        self.outputer=html_outputer.HtmlOutputer()
+    def __init__(self):
+        self.urls = url_manager.UrlManager()  # url管理器
+        self.downloader = html_downloader.HtmlDownloader()  # url下载器
+        self.parser = html_parser.HtmlParser()  # 解析器
+        self.outputer = html_outputer.HtmlOutputer()  # 输出器
 
     def craw(self, root_url):
-        count=1
-        self.urls.add_new_url(root_url) #入口url添加管理器
-        while self.urls.has_new_url(): #开启循环
+        count = 1  # 记录当前爬取的是第几个url
+        self.urls.add_new_url(root_url)  # 将入口url添加进url管理器
+        while self.urls.has_new_url():  # url管理器中有待爬取的
             try:
-                new_url=self.urls.get_new_url()
-
-                print 'craw %d : %s' %(count,new_url)
-
-                html_cont=self.downloader.download(new_url) #下载页面
-                new_urls,new_data=self.parser.parse(new_url,html_cont) #解析数据
-                self.urls.add_new_urls(new_urls) #将新url补充进管理器
-                self.outputer.collect_data(new_urls) #收集数据
-
-                if count ==1000:
+                new_url = self.urls.get_new_url()  # 获取待爬取的url
+                print 'craw %d:%s' % (count, new_url)
+                html_cont = self.downloader.download(new_url)  # 用下载器下载url页面保存到hrml——cont中
+                new_urls, new_data = self.parser.parse(new_url, html_cont)  # 解析器获得新的url管理器
+                self.urls.add_new_urls(new_urls)
+                self.outputer.collect_data(new_data)  # 收集数据
+                print new_data['title']
+                if count == 100:
                     break
-                count =count +1
-            except:
+                count = count + 1
+            except Exception, e:
+                print e
                 print 'craw failed'
-
-        self.outputer.output_html()
-
-
-if __name__=="__main__":
-    root_url="http://baike.baidu.com/view/21087.html"
-    obj_spider=SpiderMain() #主入口
-    obj_spider.craw(root_url) #启动爬虫
+        self.outputer.output_html()  # 输出收集好的数据
+        print 'finish!'
 
 
 
-
-
+if __name__ == "__main__":
+    root_url = "http://baike.baidu.com/view/21087.htm"
+    obj_spider = SpiderMain()
+    obj_spider.craw(root_url)  # 启动爬虫
